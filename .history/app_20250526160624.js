1b6e8b2488ed073b9f7875a73d9310a2252a5797
Manilamagar <manilamagar200@gmail.com>
2025-05-26 16:28:35 +0545

    first commit

---
+++ .history/app_20250526160624.js
@@ -0,0 +1,10 @@
+const express = require ("express")
+const app = express()
+// const app =require ("express")
+
+
+
+
+
+
+app.listen(3000,function)
---
+++ .history/app_20250526160645.js
@@ -0,0 +1,12 @@
+const express = require ("express")
+const app = express()
+// const app =require ("express")
+
+
+
+
+
+
+app.listen(3000,function(){
+    console.log("Backend hass ")
+})
